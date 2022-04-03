963bfc7e61b82e991cb596d878fdbef65f1f2b65
KaronisInProgram <19214686+KaronisInProgram@users.noreply.github.com>
2022-04-03 23:21:00 +0200

    extend PostgreSQL configuration

--- config/development/tools.nix
+++ config/development/tools.nix
@@ -11,12 +11,25 @@ in
 
     unstable.jetbrains.idea-community
     pkgs.android-studio
-    pkgs.postgresql_14
 
     # Language Server for Nix-Language (format, autocomplete, etc.) -> use in VSCodium Nix Extension pack
     unstable.rnix-lsp
   ];
 
   # PostgreSQL
-  services.postgresql.enable = true;
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_14;
+    enableTCPIP = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+      local all all trust
+      host all all 127.0.0.1/32 trust
+      host all all ::1/128 trust
+    '';
+    initialScript = pkgs.writeText "backend-initScript" ''
+      CREATE ROLE karonis WITH LOGIN PASSWORD 'karonis' CREATEDB;
+      CREATE DATABASE karonis;
+      GRANT ALL PRIVILEGES ON DATABASE karonis TO karonis;
+    '';
+  };
 }
